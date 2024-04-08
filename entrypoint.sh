4b6e65587def39818e71a2e602e1611204fd99d0
Adam Smith <zero1three@gmail.com>
2024-04-08 17:32:45 -0400

    feat: bind on all IPv4/IPv6 addresses

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,9 @@ if [ -f /etc/exabgp/exabgp.conf ]; then
     if [ ! -f /etc/exabgp/exabgp.env ]; then
       exabgp --fi > /etc/exabgp/exabgp.env
       # bind to all interfaces
-      sed -i "s/^bind = .*/bind = '0.0.0.0'/" /etc/exabgp/exabgp.env 
+      sed -i "s/^bind = .*/bind = '0.0.0.0 ::'/" /etc/exabgp/exabgp.env 
       # run as root (otherwise ip add commands wont work)
-      sed -i "s/^user = 'nobody'/user = 'root'/" /etc/exabgp/exabgp.env 
+      sed -i "s/^user = 'nobody'/user = 'root'/" /etc/exabgp/exabgp.env
     fi
 
     # run 
